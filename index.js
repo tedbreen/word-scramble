0a0e557588e167d00c605cff4e21290bd01f0924
Edward Breen <ted.breen@gmail.com>
2016-02-18 13:52:48 -0800

    set up React

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world!')
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var App = React.createClass({
+, render: function render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+})
