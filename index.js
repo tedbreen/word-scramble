3d7263dab66204c63bac4baf04b68508ea3c0f3b
Edward Breen <ted.breen@gmail.com>
2016-02-18 23:07:24 -0800

    App: shuffle word and check for correct answer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var keycode = require('keycode')
+var shuffleArray = require('shuffle-array')
 // components
 var Word = require('./components/word/')
 
@@ -10,14 +11,40 @@ var App = React.createClass({
   }
 
 , getInitialState: function getInitialState () {
+    var shuffledLetters = shuffleArray(this.props.word.split(''))
+
     return {
-      matchedLetters: []
-    , unmatchedLetters: this.props.word.split('')
+      shuffledLetters: shuffledLetters
+    , matchedLetters: []
+    , unmatchedLetters: shuffledLetters
     }
   }
 
 , componentDidMount: function componentDidMount () {
     document.addEventListener('keyup', this.onKeyup)
+  }
+
+, getNewWord: function getNewWord () {
+    // ajax request
+  }
+
+, checkWord: function checkWord (letters) {
+    var shuffledLetters = this.state.shuffledLetters
+    var result = letters.join('') === this.props.word
+
+    if (letters.length === shuffledLetters.length) {
+      if (result) {
+        console.log('you win')
+        this.getNewWord()
+      }
+      else {
+        console.log('you lose. try again')
+        this.setState({
+          matchedLetters: []
+        , unmatchedLetters: shuffledLetters
+        })
+      }
+    }
   }
 
 , onKeyup: function onKeyup (event) {
@@ -41,6 +68,8 @@ var App = React.createClass({
       matchedLetters: matchedLetters
     , unmatchedLetters: newUnmatchedLetters
     })
+
+    this.checkWord(matchedLetters)
   }
 
 , render: function render () {
