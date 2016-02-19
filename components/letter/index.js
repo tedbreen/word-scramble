c557d2eda2b21ba809a18a1271bd3662ffaf1034
Edward Breen <ted.breen@gmail.com>
2016-02-19 10:51:32 -0800

    Letter: add additional statuses for win/loss condition

--- components/letter/index.js
+++ components/letter/index.js
@@ -3,19 +3,34 @@ var React = require('react')
 module.exports = React.createClass({
   propTypes: {
     letter: React.PropTypes.string
-  , selected: React.PropTypes.bool
+  , status: React.PropTypes.oneOf(['matched', 'correct', 'wrong'])
   }
 
 , render: function render () {
-    var regular = {
-      backgroundColor: 'white'
+    var style = {
+      backgroundColor: ''
     }
-    var selected = {
-      backgroundColor: 'yellow'
+    var backgroundColor
+
+    switch (this.props.status) {
+      case 'matched':
+        backgroundColor = 'yellow'
+        break
+      case 'correct':
+        backgroundColor = 'green'
+        break
+      case 'wrong':
+        backgroundColor = 'red'
+        break
+      default:
+        backgroundColor = 'white'
+        break
     }
 
+    style.backgroundColor = backgroundColor
+
     return (
-      <div style={this.props.selected ? selected : regular}>
+      <div style={style}>
         <h1>{this.props.letter}</h1>
       </div>
     )
